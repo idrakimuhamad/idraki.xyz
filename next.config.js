8aec65ec1ce15f5754b558103198970b52c4d40b
Idraki Muhamad <idrakimuhamad@gmail.com>
2020-01-22 22:48:23 +0800

    update: update next-plugin-transpile-modules to next-transpile-modules
    improvement: remove the workaround now that we update to new package

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const { PHASE_PRODUCTION_SERVER } =
     ? {}
     : require('next-server/constants');
 
-function fixExternalsPlugin(config = {}) {
-  const { webpack } = config;
-  config.webpack = (config, ...rest) => {
-    config.externals = config.externals || [];
-    return webpack(config, ...rest);
-  };
-  return config;
-}
-
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_SERVER) {
     return {
@@ -20,17 +11,16 @@ module.exports = (phase, { defaultConfig }) => {
   }
 
   const withSass = require('@zeit/next-sass');
-  const withTM = require('next-plugin-transpile-modules');
+  const withTM = require('next-transpile-modules');
 
-  return fixExternalsPlugin(
-    withTM(
-      withSass({
-        target: 'serverless',
-        transpileModules: ['react-bulma-components'],
-        sassLoaderOptions: {
-          includePaths: ['./styles']
-        }
-      })
-    )
+  return withTM(
+    withSass({
+      target: 'serverless',
+      transpileModules: ['react-bulma-components'],
+      sassLoaderOptions: {
+        includePaths: ['./styles']
+      }
+    })
+    // )
   );
 };
