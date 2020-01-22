6ab0578e5aa74a2cbbdeb97ebc17bea71fe9dbd5
Idraki Muhamad <idrakimuhamad@gmail.com>
2020-01-22 22:39:47 +0800

    fix: issue with recent changes in next. added workaround so the externals are not empty.

--- next.config.js
+++ next.config.js
@@ -1,23 +1,36 @@
 const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === 'development' ? {} : require('next-server/constants')
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : require('next-server/constants');
+
+function fixExternalsPlugin(config = {}) {
+  const { webpack } = config;
+  config.webpack = (config, ...rest) => {
+    config.externals = config.externals || [];
+    return webpack(config, ...rest);
+  };
+  return config;
+}
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_SERVER) {
     return {
       /* production only config */
-    }
+    };
   }
 
-  const withSass = require('@zeit/next-sass')
-  const withTM = require('next-plugin-transpile-modules')
+  const withSass = require('@zeit/next-sass');
+  const withTM = require('next-plugin-transpile-modules');
 
-  return withTM(
-    withSass({
-      target: 'serverless',
-      transpileModules: ['react-bulma-components'],
-      sassLoaderOptions: {
-        includePaths: ['./styles']
-      }
-    })
-  )
-}
+  return fixExternalsPlugin(
+    withTM(
+      withSass({
+        target: 'serverless',
+        transpileModules: ['react-bulma-components'],
+        sassLoaderOptions: {
+          includePaths: ['./styles']
+        }
+      })
+    )
+  );
+};
