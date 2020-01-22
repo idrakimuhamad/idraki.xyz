1eb9f2fcb1394d1bde06db07821d8a8b49898043
Idraki Muhamad <idrakimuhamad@gmail.com>
2020-01-22 23:23:13 +0800

    improvement: remove deprecated Container

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import App, { Container } from 'next/app'
-import Head from 'next/head'
-import '../styles/style.scss'
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import '../styles/style.scss';
 
 class RootApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {}
+    let pageProps = {};
 
     if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
+      pageProps = await Component.getInitialProps(ctx);
     }
 
-    return { pageProps }
+    return { pageProps };
   }
 
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps } = this.props;
     return (
-      <Container>
+      <>
         <Head>
           <title>Idraki Muhamad: JavaScript Developer</title>
         </Head>
         <Component {...pageProps} />
-      </Container>
-    )
+      </>
+    );
   }
 }
 
-export default RootApp
+export default RootApp;
