90124de0d9afd46a6dbaeaefe9437c980bcb094b
Idraki Muhamad <idrakimuhamad@gmail.com>
2020-10-29 16:34:17 +0800

    chore: init with tailwindcss template

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,7 @@
-import React from 'react';
-import App from 'next/app';
-import Head from 'next/head';
-import '../styles/style.scss';
+import '../styles/index.css'
 
-class RootApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Head>
-          <title>Idraki Muhamad: JavaScript Developer</title>
-        </Head>
-        <Component {...pageProps} />
-      </>
-    );
-  }
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
-export default RootApp;
+export default MyApp
